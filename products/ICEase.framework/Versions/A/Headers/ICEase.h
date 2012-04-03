e6121acd3dceccea47ca357c8bd36729550a7492
Matthew Casey <mcasey@iosconsultancy.co.uk>
2012-04-03 16:02:22 +0100

    Updated ReadMe with Install Instructions and added products folder

---
+++ products/ICEase.framework/Versions/A/Headers/ICEase.h
@@ -0,0 +1,11 @@
+//
+//  ICEase.h
+//  ICEase
+//
+//  Created by Matthew Casey on 03/04/2012.
+//  Copyright (c) 2012 iOSConsultancy.co.uk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ICEaseLogCentre.h"
+#import "NSObject+LoggingAdditions.h"
