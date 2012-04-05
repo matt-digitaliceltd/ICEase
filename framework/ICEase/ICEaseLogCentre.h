c5dbf8a200b5717528edad05b7578aaea01e6bde
Matthew Casey <mcasey@iosconsultancy.co.uk>
2012-04-05 19:46:32 +0100

    Minor changes

--- framework/ICEase/ICEaseLogCentre.h
+++ framework/ICEase/ICEaseLogCentre.h
@@ -12,7 +12,6 @@ typedef enum ICEaseLogLevel {
     ICEaseLogLevelError,
     ICEaseLogLevelWarning,
     ICEaseLogLevelDebug
-    
 } ICEaseLogLevel;
 @interface ICEaseLogCentre : NSObject
 ////////////////////////////////////////////////////////////////////////////////
